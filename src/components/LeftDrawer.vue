e16ae7ab7977848b65a76548a48dd34c563cfb4d
arpit singh <singharpit0911@gmail.com>
2022-01-07 17:30:55 +0530

    router setup and trending page

--- src/components/LeftDrawer.vue
+++ src/components/LeftDrawer.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-item class="q-pa-md" clickable tag="a" :href="link">
+  <q-item class="q-pa-md" clickable  @click="openTab">
     <q-item-section v-if="icon" avatar>
       <q-icon :name="icon" color="grey" size="md" />
     </q-item-section>
@@ -14,5 +14,11 @@
 export default {
   name: "LeftDrawer",
   props: ["title", "icon", "link"],
+  methods:{
+    openTab(){
+      console.log("clicked tab", this.link);
+      this.$router.push(this.link)
+    }
+  }
 };
 </script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,27 @@
-
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
+    path: "",
+    component: () => import("layouts/MainLayout.vue"),
     children: [
-      { path: '', component: () => import('src/pages/Home.vue') }
-    ]
+      {
+        path: "",
+        name: 'home',
+        caseSensitive: true,
+        component: () => import("src/pages/Home.vue"),
+      },
+      {
+        path: "trending",
+        component: () => import("src/pages/Trending.vue"),
+      },
+    ],
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/Error404.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/Error404.vue"),
+  },
+];
 
-export default routes
+export default routes;
